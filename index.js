3d03d0dfa8aaa990fffc7270459492b3c1657bd4
ghstN <98927555+ghstN@users.noreply.github.com>
2022-04-18 22:01:39 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-$('#btPagina').click(
+$.support.cors = true;
+
+$(document).ready(
     function(){
-        alert ('Hello World');
+        $('#conteudo').load("pages/pagina1.html")
     }
     
+);
+
+
+$('#BtPágina1').click(
+    function(){
+        alert('Hello, JAVA');
+    }
 )
+$('#BtPágina2').click(
+    function(){
+        //console.log('Clique do botão 2');
+        $('#conteudo').load("pages/pagina2.html")
+    }
+)
+$('#BtPágina3').click(
+    function(){
+        $('#conteudo').load("pages/pagina3.html")
+    }
+)
+
+
